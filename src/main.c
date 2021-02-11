97bcb28479bccd27ab28fb3b13d74fa59fad1bc7
user-friendly <pivanov@foobar.ninja>
2021-02-11 20:21:01 +0200

    Struct memory fun

--- src/main.c
+++ src/main.c
@@ -40,7 +40,14 @@ int main (int argc, char *argv[]) {
 	printf("Size of      [T]: %zu\n", sizeof(T));
 	printf("Alignment of [T]: %zu\n", alignof(T));
 
-	T x = {'a', 8, 1.23456789e10};
+	printf("Alignment of [char]: %zu\n", alignof(char));
+	printf("Alignment of [int]: %zu\n", alignof(int));
+	printf("Alignment of [long]: %zu\n", alignof(long));
+	printf("Alignment of [float]: %zu\n", alignof(float));
+	printf("Alignment of [double]: %zu\n", alignof(double));
+	printf("Alignment of [void*]: %zu\n", alignof(void*));
+
+	T x = {'a', 1024, 1.23456789e10};
 
 	printf(
 		"Offset [T.a]: %zu\n" \
@@ -60,6 +67,14 @@ int main (int argc, char *argv[]) {
 		&x.c
 	);
 
+	// Silly way of setting the value of T.b to 7. Works only for little-endian.
+	char *p = &x.a;
+	p += 4;
+	*p = 7; p++;
+	*p = 0; p++;
+	*p = 0; p++;
+	*p = 0;
+
 	printf(
 		"Value [T.a]: %c\n" \
 		"Value [T.b]: %d\n" \
@@ -68,13 +83,6 @@ int main (int argc, char *argv[]) {
 		x.b,
 		x.c
 	);
-
-//	printf("Alignment of [char]: %zu\n", alignof(char));
-//	printf("Alignment of [int]: %zu\n", alignof(int));
-//	printf("Alignment of [long]: %zu\n", alignof(long));
-//	printf("Alignment of [float]: %zu\n", alignof(float));
-//	printf("Alignment of [double]: %zu\n", alignof(double));
-//	printf("Alignment of [void*]: %zu\n", alignof(void*));
 
 	return EXIT_SUCCESS;
 }
