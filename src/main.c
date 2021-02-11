4528105826d5b743210e36f3c5dd7b277cfea7ed
user-friendly <pivanov@foobar.ninja>
2021-02-11 18:56:04 +0200

    Fun with alignment

--- src/main.c
+++ src/main.c
@@ -5,6 +5,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdalign.h>
+#include <stddef.h>
+
+void recurse() {
+	recurse();
+}
 
 /**
  * Program entry point.
@@ -12,6 +18,63 @@
  * @see https://en.cppreference.com/w/c/language/main_function
  */
 int main (int argc, char *argv[]) {
-	printf("This GNU Autotools skeleton project works!\n");
+	printf("This GNU Autotools skeleton project works!\n\n");
+
+//	char *buffer = (char *) malloc(1024*1024);
+//
+//
+//
+//	free(buffer);
+
+	// recurse();
+
+	typedef struct T T;
+	struct T {
+		char 	a; // 1
+		int 	b; // 4
+		double  c; // 8
+				   // total unpadded:
+				   // 13
+	};
+
+	printf("Size of      [T]: %zu\n", sizeof(T));
+	printf("Alignment of [T]: %zu\n", alignof(T));
+
+	T x = {'a', 8, 1.23456789e10};
+
+	printf(
+		"Offset [T.a]: %zu\n" \
+		"Offset [T.b]: %zu\n" \
+		"Offset [T.c]: %zu\n",
+		offsetof(T, a),
+		offsetof(T, b),
+		offsetof(T, c)
+	);
+
+	printf(
+		"Address [T.a]: %p\n" \
+		"Address [T.b]: %p\n" \
+		"Address [T.c]: %p\n",
+		&x.a,
+		&x.b,
+		&x.c
+	);
+
+	printf(
+		"Value [T.a]: %c\n" \
+		"Value [T.b]: %d\n" \
+		"Value [T.c]: %.4e\n",
+		x.a,
+		x.b,
+		x.c
+	);
+
+//	printf("Alignment of [char]: %zu\n", alignof(char));
+//	printf("Alignment of [int]: %zu\n", alignof(int));
+//	printf("Alignment of [long]: %zu\n", alignof(long));
+//	printf("Alignment of [float]: %zu\n", alignof(float));
+//	printf("Alignment of [double]: %zu\n", alignof(double));
+//	printf("Alignment of [void*]: %zu\n", alignof(void*));
+
 	return EXIT_SUCCESS;
 }
